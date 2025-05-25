e70860a71d3f57886acb7b11584c53fee8bd2245
PW-EricPhungVietNam <vominhphung.vip@gmail.com>
2025-05-25 18:37:01 +0700

    add: lesson4

--- lesson-4/01-forEach.js
+++ lesson-4/01-forEach.js
@@ -7,6 +7,12 @@ function inRaTungPhanTu(phanTu) {
 }
 numbers.forEach(inRaTungPhanTu);
 
+// 1.1 Cách 2: In lần lượt từng phần tử của numbers.
+numbers.forEach(cach2 => {
+    console.log(cach2);
+});
+
+
 // 1.2 Tính tổng, tìm giá trị lớn nhất và nhỏ nhất của numbers
 let sum = 0;
 numbers.forEach(
@@ -26,10 +32,32 @@ function timGiaTri(soCanTim) {
 numbers.forEach(timGiaTri)
 console.log(`Giá trị lớn nhất là: ${max}`, `Giá trị nhỏ nhất là: ${min}`)
 
+//1.2 Cách 2:
+let total = 0;
+let minNumber = numbers[0];
+let maxNumber = numbers[0];
+
+numbers.forEach(numC => {
+    total += numC
+
+    if (numC > maxNumber) {
+        maxNumber = numC
+    }
+
+    if (numC < minNumber) {
+        minNumber = numC
+    }
+});
+console.log(total);
+console.log(minNumber);
+console.log(maxNumber);
 
 
 // 1.3 Tạo mảng mới từ numbers, mỗi phần tử nhân đôi
-function moiPhanTuNhan2(num) { return num * 2 }
-const mangMoi = numbers.map(moiPhanTuNhan2)
-console.log(mangMoi)
+let doubled = [];
+
+numbers.forEach (num => {
+    doubled.push(num*2)
+});
+console.log(doubled);
 
